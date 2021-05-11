b24af2f4c629a4ebe527c177faaa0edfb0b4ffca
Denis Zhurov <den_fobos@mail.ru>
2021-05-11 20:56:13 +0300

    added storing images on localStorage

--- 8_final.js
+++ 8_final.js
@@ -9,6 +9,25 @@ function useRequest(url) {
     .catch(() => console.log('Fetch error'))
 }
 
+function loadImage(url) {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = url;
+        img.crossOrigin = 'anonymous';
+        img.alt = "";
+        img.addEventListener('load', () => resolve(img));
+        img.addEventListener('error', () => reject(new Error("Image can't be loaded")));
+    });
+}
+
+function makeCanvas(img) {
+    const canvas = document.createElement('CANVAS');
+    const scale = Math.min(canvas.width / img.width, canvas.height / img.height);
+    const contex = canvas.getContext('2d');
+    contex.drawImage(img, 0, 0, img.width * scale, img.height * scale);
+    return canvas
+}
+
 function listMaker(valueArr) {
     const list = document.createElement('UL');
     list.style.textAlign = 'left';
@@ -18,14 +37,28 @@ function listMaker(valueArr) {
         const span = document.createElement('SPAN');
         const a = document.createElement('A');
         const img = document.createElement('IMG');
+        let url;
+        //check local stored images and load them
+        if(localStorage.getItem(`${item.id}`)) url = `${localStorage.getItem(item.id)}`;
+        else url = `${item.download_url}`;
 
-        img.src = `${item.download_url}`;
-        img.style.height = '200px';
-        img.style.width = 'auto';
+        //load image, make canvas from it and insert it to <a></a> element
+        const imgPromis = loadImage(url)
+        imgPromis
+        .then(function(resolve) {
+            let canvas = makeCanvas(resolve);
+            a.appendChild(canvas);
+            const id = item.id;
+            //caching loaded images to localStorage
+            if (!localStorage.getItem(id)) localStorage.setItem(`${id}`, canvas.toDataURL());
+        })
+        .catch(error => {
+            console.log(error);
+            a.append(error);
+        })
 
         a.href = `${item.download_url}`;
         a.target = '_blank';
-        a.appendChild(img);
 
         span.textContent = `${item.author}`;
 
@@ -54,9 +87,9 @@ btnRequest.addEventListener('click', async() =>{
     const limit = limitInp.value;
     const url = `https://picsum.photos/v2/list?page=${page}&limit=${limit}`;
     const data = await useRequest(url);
-    //do ifs
     localStorage.clear();
-    localStorage.setItem('data', JSON.stringify(data))
+    localStorage.setItem('data', JSON.stringify(data));
+    //do ifs
     const list = listMaker(data);
     display(list);
 });
